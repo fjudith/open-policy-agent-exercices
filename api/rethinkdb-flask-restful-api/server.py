2133ab6066ab5a8e2b41c39ddc68497f42e33bc7
Florian JUDITH <florian.judith.b@gmail.com>
2021-03-05 20:51:56 +0100

    Added before/teadown request for future opa integration

--- api/rethinkdb-flask-restful-api/server.py
+++ api/rethinkdb-flask-restful-api/server.py
@@ -1,18 +1,39 @@
 from flasgger import Swagger
-from flask import Flask
+from flask import Flask, abort, g
 from flask.blueprints import Blueprint
 
-from config import args
+from config import args, logger
 import routes
 from models import rdb
+
+from rethinkdb.errors import RqlDriverError
+
+server = Flask(__name__)
+
+# open connection before each request
+@server.before_request
+def before_request():
+    logger.debug('Connecing to database host:"{0}"'.format(args.rethinkdb_host))
+    try:
+        g.rdb_conn = rdb.connect(host=args.rethinkdb_host, port=args.rethinkdb_port, db=args.rethinkdb_database)
+    except RqlDriverError:
+        abort(503, "Database connection could be established.")
+
+# close the connection after each request
+@server.teardown_request
+def teardown_request(exception):
+    logger.debug('Disconnecting from database host:"{0}"'.format(args.rethinkdb_host))
+    try:
+        g.rdb_conn.close()
+    except AttributeError:
+        pass
+
 
 # config your API specs
 # you can define multiple specs in the case your api has multiple versions
 # ommit configs to get the default (all views exposed in /spec url)
 # rule_filter is a callable that receives "Rule" object and
 #   returns a boolean to filter in only desired views
-
-server = Flask(__name__)
 
 server.config["SWAGGER"] = {
     "swagger_version": "2.0",
